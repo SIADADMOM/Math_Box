37bc47a944413e5f8db0f44171f01b945cf5e541
codeit <realk12345@gmail.com>
2023-04-25 16:17:15 +0900

    Add get_Sum funciton from test branch

--- calculator.py
+++ calculator.py
@@ -14,6 +14,7 @@ def divide_new(a, b):
  def getMedian(a, b):
     return (a+b/2)
 
+<<<<<<< HEAD
  def getRemainder(a, b):
     return a//b
 
@@ -26,3 +27,7 @@ def get_Abs(num):
 def get_Percent(a, b):
     return (a/b) * 100
 
+=======
+def getSum_ver1(n):
+    return n(n+1)/2
+>>>>>>> 30b6b7d (Add get_Sum_ver1 function)
